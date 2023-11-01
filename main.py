5aa402d33358c29705d929e9b9062603b5f160e8
Karaseyka <vladchuprina12@mail.ru>
2023-11-01 20:57:39 +0300

    made list with added olimps

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import sys
 import csv
 import sqlite3
 import datetime
-
 from PyQt5 import uic
 from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidgetItem, QHBoxLayout, QLabel, QWidget, QVBoxLayout, \
     QFrame, QMessageBox, QListWidget
 
 
-class MyWidget(QMainWindow):
+class WelcomePG(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('start.ui', self)
@@ -65,6 +64,11 @@ class MainPG(QMainWindow):
         self.birth.setDate(date_time_obj.date())
         self.savech.clicked.connect(self.save)
 
+        self.for_list_added()
+        self.listWidget2.setStyleSheet('QListWidget::item { border: 1px solid black; } QListWidget::item:selected { '
+                                       'background: lightblue; }')
+        self.listWidget2.itemClicked.connect(self.run_added)
+
     def save(self):
         connection = sqlite3.connect('olimp.sqlite')
         cursor = connection.cursor()
@@ -79,13 +83,27 @@ class MainPG(QMainWindow):
         for ship_data in self.olimps:
             item = QListWidgetItem()
             item.setSizeHint(QSize(200, 100))
-            widget = get_item_wight(ship_data)  # Call the above function to get the corresponding
-            self.listWidget.addItem(item)  # Add item
+            widget = get_item_wight(ship_data)
+            self.listWidget.addItem(item)
             self.listWidget.setItemWidget(item, widget)
+
+    def for_list_added(self):
+        connection = sqlite3.connect('olimp.sqlite')
+        cursor = connection.cursor()
+        added_olimps = cursor.execute("""SELECT name, olid, date, time FROM olimpsus, status
+        WHERE status.id = olimpsus.status""").fetchall()
+        connection.commit()
+        connection.close()
+        for ship_data in added_olimps:
+            item = QListWidgetItem()
+            item.setSizeHint(QSize(200, 100))
+            widget = get_item_added(ship_data, self.olimps)
+            self.listWidget2.addItem(item)
+            self.listWidget2.setItemWidget(item, widget)
 
     def run(self, item):
         x = self.listWidget.indexFromItem(item).row()  # индекс выбранного элемента
-        self.to_prewatch = Prewatch(self.olimps[x])
+        self.to_prewatch = Prewatch(self.olimps[x], x, self, self.listWidget2)
         if len(self.que) == 0:
             self.que.append(self.to_prewatch)
             self.to_prewatch.show()
@@ -94,14 +112,42 @@ class MainPG(QMainWindow):
             self.que = [self.to_prewatch]
             self.to_prewatch.show()
 
+    def run_added(self):
+        pass
+
 
 class Prewatch(QMainWindow):
-    def __init__(self, olimp):
+    def __init__(self, olimp, num, mn, lw):
         super().__init__()
         uic.loadUi("prewatch.ui", self)
+        self.cur_status = "0"
         self.olimp = olimp
+        self.num = num
+        self.mn = mn
+        self.lw = lw
         self.olimpName.setText(olimp[0])
+        self.olimpDesc.setText("Описание: " + olimp[1])
+        self.olimpLev.setText("Уровень: " + olimp[2])
+        self.profil.setText("Профиль: " + olimp[3])
+        self.connection = sqlite3.connect('olimp.sqlite')
+        self.cur = self.connection.cursor()
+        st = self.cur.execute("SELECT * FROM status").fetchall()
+        self.connection.commit()
+        for i in st:
+            self.comboBox.addItem(i[1])
+        self.comboBox.activated.connect(self.onActivated)
+        self.accept.clicked.connect(self.run)
 
+    def run(self):
+        self.cur.execute(f'INSERT INTO olimpsus (status, olid, date, time) VALUES (?, ?, ?, ?)',
+                         (int(self.cur_status) + 1, int(self.num), self.dateEdit.text(), self.timeEdit.text()))
+        self.connection.commit()
+        self.lw.clear()
+        self.mn.for_list_added()
+        self.close()
+
+    def onActivated(self, text):
+        self.cur_status = text
 
 
 def get_item_wight(olimp):
@@ -111,6 +157,17 @@ def get_item_wight(olimp):
     layout_main.addWidget(QLabel("Описание: " + olimp[1]))
     layout_main.addWidget(QLabel("Уровень: " + olimp[2]))
     layout_main.addWidget(QLabel("Профиль: " + olimp[3]))
+    frame.setLayout(layout_main)
+    return frame
+
+
+def get_item_added(olimp, olimps):
+    frame = QFrame()
+    layout_main = QVBoxLayout()
+    layout_main.addWidget(QLabel(olimps[olimp[1]][0]))
+    layout_main.addWidget(QLabel("Дата: " + olimp[2]))
+    layout_main.addWidget(QLabel("Время: " + olimp[3]))
+    layout_main.addWidget(QLabel("Статаус: " + olimp[0]))
     frame.setLayout(layout_main)
     return frame
 
@@ -131,11 +188,12 @@ def csv_getter():
 
 
 def get_user():
-    con = sqlite3.connect("olimp.sqlite")
-    cur = con.cursor()
+    connection = sqlite3.connect('olimp.sqlite')
+    cur = connection.cursor()
     user = cur.execute("SELECT * FROM user").fetchall()
-    con.commit()
-    con.close()
+    connection.commit()
+    connection.close()
+
     if len(user) != 0:
         return user[0]
     return False
@@ -144,7 +202,7 @@ def get_user():
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     if not get_user():
-        ex = MyWidget()
+        ex = WelcomePG()
     else:
         ex = MainPG()
     ex.show()
